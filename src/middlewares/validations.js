bd3fb5e12527a89a6821505add97d6bb5602a7e9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 15:51:05 -0300

    feat: Set email validation

---
+++ src/middlewares/validations.js
@@ -0,0 +1,19 @@
+const validateEmail = (req, res, next) => {
+  const { email } = req.body;
+
+  const emailRegex = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
+  }
+
+  if (emailRegex.test(email)) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  return next();
+};
+
+module.exports = {
+  validateEmail,
+};
