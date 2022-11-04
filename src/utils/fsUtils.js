8330617c1ce13bf34b1371938f473e73e51bfe3b
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 15:02:10 -0300

    chore: Take out console.log

--- src/utils/fsUtils.js
+++ src/utils/fsUtils.js
@@ -6,7 +6,6 @@ const path = '../talker.json';
 const readFile = async () => {
   try {
     const response = await fs.readFile(join(__dirname, path));
-    console.log(JSON.parse(response));
     return JSON.parse(response);
   } catch (error) {
     console.log(error);
