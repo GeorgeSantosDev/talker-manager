f7cfe96ad39de2303213b4be577337fff3017be9
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 15:00:37 -0300

    feat: Add get method to  path /talker

--- src/routers/talkerRoutes.js
+++ src/routers/talkerRoutes.js
@@ -1,5 +1,11 @@
 const express = require('express');
+const { readFile } = require('../utils/fsUtils');
 
 const route = express.Router();
+
+route.get('/talker', async (_req, res) => {
+  const response = await readFile();
+  res.status(200).json(response);
+});
 
 module.exports = route;
