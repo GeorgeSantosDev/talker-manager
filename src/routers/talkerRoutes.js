44900acefa922a4e7309c8daab18382cc7e5c693
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 17:50:16 -0300

    feat: Add post method to path /talker

--- src/routers/talkerRoutes.js
+++ src/routers/talkerRoutes.js
@@ -1,12 +1,18 @@
 const express = require('express');
-const { readFile } = require('../utils/fsUtils');
+const { readFile, writeFile } = require('../utils/fsUtils');
 const { getById } = require('../utils/handleTalkerAndLogin');
+const { validateToken,
+  validateName, 
+  validateAge,
+  validateTalk,
+  validateDate,
+  validateRate } = require('../middlewares/validations');
 
 const route = express.Router();
 
 route.get('/talker', async (_req, res) => {
-  const response = await readFile();
-  res.status(200).json(response);
+  const talkers = await readFile();
+  res.status(200).json(talkers);
 });
 
 route.get('/talker/:id', async (req, res) => {
@@ -19,6 +25,37 @@ route.get('/talker/:id', async (req, res) => {
   }
 
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
-  });
+});
+
+route.post('/talker',
+  validateToken,
+  validateName,
+  validateAge,
+  validateTalk,
+  validateDate,
+  validateRate, async (req, res) => {
+    const newTalker = req.body;
+    const talkers = await readFile();
+
+    await writeFile([...talkers, newTalker]);
+
+  res.status(201).json(newTalker);
+});
+
+// route.put('/talker',
+//   validateToken,
+//   validateName,
+//   validateAge,
+//   validateTalk,
+//   validateDate,
+//   validateRate, async (req, res) => {
+//     const talkerUpdate = req.body;
+//     const talkers = await readFile();
+
+
+//     await writeFile([...talkers, newTalker]);
+
+//   res.status(200).json(newTalker);
+// });
 
 module.exports = route;
