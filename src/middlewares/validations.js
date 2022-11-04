0c8ed4dc9c16758ee7639d659639a36381d44903
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 16:00:15 -0300

    feat: Set validatePassword function

--- src/middlewares/validations.js
+++ src/middlewares/validations.js
@@ -7,8 +7,23 @@ const validateEmail = (req, res, next) => {
     return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
 
-  if (emailRegex.test(email)) {
+  if (!emailRegex.test(email)) {
     return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+
+  return next();
+};
+
+const validatePassword = (req, res, next) => {
+  const MINIMUM_PASSWORD_LENGTH = 6;
+  const { password } = req.body;
+
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+
+  if (password.length < MINIMUM_PASSWORD_LENGTH) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
   }
 
   return next();
@@ -16,4 +31,5 @@ const validateEmail = (req, res, next) => {
 
 module.exports = {
   validateEmail,
+  validatePassword,
 };
