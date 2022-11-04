7f8dad582dde10ccb905c01c12a29a4680af8cb7
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 15:35:39 -0300

    feat: Add post method to path /login

--- src/routers/loginRoutes.js
+++ src/routers/loginRoutes.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const { generateToken } = require('../utils/handleTalkerAndLogin');
 
 const route = express.Router();
+
+route.post('/login', (req, res) => {
+  res.status(200).json({ token: generateToken() });
+});
 
 module.exports = route;
