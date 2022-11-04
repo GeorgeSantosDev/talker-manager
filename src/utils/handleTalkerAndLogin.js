de266721ecdb1ee804c944ebc1a010c2ae70a340
GeorgeSantosDev <george.santos_ufmg@outlook.com>
2022-11-04 19:19:21 -0300

    feat: Set get talker by name function

--- src/utils/handleTalkerAndLogin.js
+++ src/utils/handleTalkerAndLogin.js
@@ -10,7 +10,14 @@ const rand = () => Math.random().toString(36).slice(3, 11);
 
 const generateToken = () => rand() + rand();
 
+const getByName = async (name) => {
+  const file = await readFile();
+
+  return file.filter((talker) => talker.name.toUpperCase().includes(name.toUpperCase()));
+};
+
 module.exports = {
   getById,
   generateToken,
+  getByName,
 };
